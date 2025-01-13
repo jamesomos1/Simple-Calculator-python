b2662c8ba199901ed3b463551b2ce30c2016a6b4
jamesomos1 <159488347+jamesomos1@users.noreply.github.com>
2025-01-13 15:36:49 -0500

    Delete inputs.py

--- inputs.py
+++
@@ -1,9 +0,0 @@
-
-name = input("Enter your name: ")
-age = int(input("How old are you?:  "))
-food = input("What do you love to eat?: ")
-age = age + 1
-
-print(f"Wassup {name}")
-print(f"You are {age} years old")
-print(f'I love to eat {food} too my guy')
