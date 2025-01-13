dcb2ebba73ef71fa536227a5c796693d69f05e6a
jamesomos1 <159488347+jamesomos1@users.noreply.github.com>
2025-01-13 15:37:47 -0500

    Delete store.py

--- store.py
+++
@@ -1,9 +0,0 @@
-
-item = input("What item will you like to buy?:  ")
-price = float(input("What is the price?: "))
-quantity = int(input("How many would you like?: "))
-
-total = price * quantity
-
-print(f"Yu have bought {quantity} x {item}(s)")
-print(f"Your total is: ${round(total, 2)}")
